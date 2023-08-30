88eb193581eab5ef09f2b700b9fd26bb0c2a805c
MrDuybinh2111 <143157072+MrDuybinh2111@users.noreply.github.com>
2023-08-30 08:15:41 +0700

    first commit

---
+++ sever.py
@@ -0,0 +1,6 @@
+#gửi api +file lên server thì server return lại json data báo gửi thành công, client sẽ gửi file tiếp theo.
+#Data gửi lên tuần tự từng file
+#Em viết code tạo file đó, có thể  lưu đường dẫn , thống tin file, nội dung file Trên MySQL
+#Nội dung file đó:
+#'2023-08-15 10:35:04',0,0,0,50,23,55
+#các giá trị random. 5p tạo 1 file, để nhanh thì 5s tạo file 1 lần. sau này hệ thống log 5p
